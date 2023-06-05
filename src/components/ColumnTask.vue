7d1f2012b27fe2abc3a77d07b403f9b6fedfd8ec
Sudhanshu Chaudhary <sudhanshulicensed>
2023-06-05 10:29:20 +0530

    Used Mixins to remove repeated functions

--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -18,26 +18,17 @@
 </template>
 
 <script>
+import movingTasksAndColumnsMixin from "@/mixins/movingTasksAndColumnsMixin";
+
 export default {
+  mixins: [movingTasksAndColumnsMixin],
   props: {
-    board: {
-      type: Object,
-      required: true,
-    },
     task: {
       type: Object,
       required: true,
     },
     taskIndex: {
       type: Number,
-      required: true,
-    },
-    columnIndex: {
-      type: Number,
-      required: true,
-    },
-    column: {
-      type: Object,
       required: true,
     },
   },
@@ -52,36 +43,6 @@ export default {
       event.dataTransfer.setData("from-task-index", taskIndex);
       event.dataTransfer.setData("type", "task");
       event.dataTransfer.setData("from-column-index", fromColumnindex);
-    },
-    moveTaskOrColumn(event, toTasks, toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData("type");
-      if (type === "task") {
-        this.moveTask(
-          event,
-          toTasks,
-          toTaskIndex !== undefined ? toTaskIndex : toTasks.length
-        );
-      } else {
-        this.moveColumn(event, toColumnIndex);
-      }
-    },
-    moveTask(event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      const fromTaskIndex = event.dataTransfer.getData("from-task-index");
-      this.$store.commit("MOVE_TASK", {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex,
-      });
-    },
-    moveColumn(event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      this.$store.commit("MOVE_COLUMN", {
-        fromColumnIndex,
-        toColumnIndex,
-      });
     },
   },
 };
